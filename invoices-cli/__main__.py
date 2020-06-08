54b44e723c6a1482a448be5a73a94aee0a6f3f12
Nathan Lovato <nathan@gdquest.com>
2020-06-08 16:04:58 -0600

    Clean up code in __main__ and cli

--- invoices-cli/__main__.py
+++ invoices-cli/__main__.py
@@ -4,6 +4,7 @@ import itertools
 import json
 import locale
 import os
+from os.path import join, exists, dirname, isfile, splitext, basename
 import shutil
 
 from .modules.command_line import parse_and_get_arguments
@@ -12,65 +13,73 @@ from .modules.invoice import InvoiceList, InvoiceTemplate
 from .modules.render import render
 
 DEBUG = True
-THIS_FILE_PATH = os.path.dirname(__file__)
+THIS_FILE_PATH = dirname(__file__)
 
 
 def get_data_from_json(path):
     assert path.endswith(".json")
     with open(path) as data:
         return json.loads(data.read())
+    return ""
 
 
 def set_up_output_directory(path):
     """Create output directory tree and copy the assets required to render the pdfs
     """
-    html_path = os.path.join(path, "html")
-    if not os.path.exists(html_path):
+    html_path = join(path, "html")
+    if not exists(html_path):
         os.makedirs(html_path)
-    css_output_path = os.path.join(html_path, "style.css")
-    css_file_path = os.path.join(THIS_FILE_PATH, "template/style.css")
+    css_output_path = join(html_path, "style.css")
+    css_file_path = join(THIS_FILE_PATH, "template/style.css")
     shutil.copy(css_file_path, css_output_path)
-    img_output_path = os.path.join(html_path, "img")
-    if not os.path.exists(img_output_path):
-        img_file_path = os.path.join(THIS_FILE_PATH, "template/img")
+    img_output_path = join(html_path, "img")
+    if not exists(img_output_path):
+        img_file_path = join(THIS_FILE_PATH, "template/img")
         shutil.copytree(img_file_path, img_output_path)
 
 
 def save_html_files(dir_out, htmls, filenames):
     """Saves each html stream from the htmls list as a file"""
-    html_directory = os.path.join(dir_out, "html")
+    html_directory = join(dir_out, "html")
     for html, filename in zip(htmls, filenames):
-        export_path = os.path.join(html_directory, filename + ".html")
+        export_path = join(html_directory, filename + ".html")
         with codecs.open(export_path, "w", encoding="utf-8") as invoice_file:
             invoice_file.writelines(html)
+
+
+def get_config():
+    """
+    Returns a Config object with information about the company and invoice settings.
+    """
+    config_path = join(THIS_FILE_PATH, "./data/config.json")
+    config = Config(config_path)
+
+    json_path = join(THIS_FILE_PATH, "./data/company.json")
+    company = get_data_from_json(json_path)
+    config.set("company", company)
+    config.set("payment_paypal", "PayPal address: " + company["paypal"])
+    bank_details_path = join(THIS_FILE_PATH, "template/bank-details.html")
+    with codecs.open(bank_details_path, "r", encoding="utf-8") as html_doc:
+        config.set("payment_wire", html_doc.read())
+    return config
 
 
 def main():
     locale.setlocale(locale.LC_ALL, "")
 
-    config_path = os.path.join(THIS_FILE_PATH, "./data/config.json")
-    config = Config(config_path)
-    json_path = os.path.join(THIS_FILE_PATH, "./data/company.json")
-    company = get_data_from_json(json_path)
+    config = get_config()
 
-    config.set("payment_paypal", "PayPal address: " + company["paypal"])
-    bank_details_path = os.path.join(THIS_FILE_PATH, "template/bank-details.html")
-    with codecs.open(bank_details_path, "r", encoding="utf-8") as html_doc:
-        config.set("payment_wire", html_doc.read())
-
-    template = InvoiceTemplate(
-        os.path.join(THIS_FILE_PATH, "template/invoice.html"), company
-    )
+    template_path = join(THIS_FILE_PATH, "template/invoice.html")
+    template = InvoiceTemplate(template_path, config.get("company"))
     if template.is_invalid():
         return
 
     args = parse_and_get_arguments(config)
-    db_file_path = args.path
-    assert os.path.isfile(db_file_path)
-    db_file_name = os.path.splitext(os.path.basename(db_file_path))[0]
-    dir_out = os.path.join(config.get("output_path"), db_file_name)
+    assert isfile(args.path)
+    db_file_name = splitext(basename(args.path))[0]
+    dir_out = join(config.get("output_path"), db_file_name)
 
-    invoice_list = InvoiceList(db_file_path)
+    invoice_list = InvoiceList(args.path)
     invoice_list.parse_csv(config)
     htmls = map(
         template.get_invoices_as_html, invoice_list.db, itertools.repeat(config)
--- invoices-cli/modules/command_line.py
+++ invoices-cli/modules/command_line.py
@@ -9,13 +9,17 @@ def parse_and_get_arguments(config: Config) -> Namespace:
         prog="invoices", description="Creates PDF invoices from CSV tables"
     )
     parser.add_argument(
-        "-p", "--path", default=config.get("database_path"), help="Path to the invoices database to render."
+        "-p",
+        "--path",
+        default=config.get("database_path"),
+        help="Path to the invoices database to render.",
     )
 
     subparsers = parser.add_subparsers(dest="command", required=True)
     parser_config = subparsers.add_parser(
         "config",
-        help="Commands related to the program's configuration. Set, get values, or write the default configuration to the disk.",
+        help="Commands related to the program's configuration."
+        "Set, get values, or write the default configuration to the disk.",
     )
     parser_config.add_argument("-s", "--set", help="Set an option to a given value.")
     parser_config.add_argument("-g", "--get", help="Get the value of a given option")
@@ -26,22 +30,26 @@ def parse_and_get_arguments(config: Config) -> Namespace:
     parser_generate.add_argument(
         "-o",
         "--out-path",
-        help="Directory to generate html files into. Overrides the value from your configuration file.",
+        help="Directory to generate html files into."
+        "Overrides the value from your configuration file.",
     )
     parser_generate.add_argument(
         "-t",
         "--template-path",
-        help="Path to an html file to use as a template for the invoices. Overrides the value from your configuration file.",
+        help="Path to an html file to use as a template for the invoices."
+        "Overrides the value from your configuration file.",
     )
 
     parser_render = subparsers.add_parser("render", help="command render")
     parser_render.add_argument(
         "--as-pdf",
-        help="Render the invoices as PDF files. Requires the program wkhtmltopdf to render the files.",
+        help="Render the invoices as PDF files."
+        "Requires the program wkhtmltopdf to render the files.",
     )
     parser_render.add_argument(
         "--as-png",
-        help="Render the invoices as PNG files. Requires the program wkhtmltopdf to render the files.",
+        help="Render the invoices as PNG files."
+        "Requires the program wkhtmltopdf to render the files.",
     )
 
     return parser.parse_args()
